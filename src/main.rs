53e1e2753fc722be014eae0b02d106b4835b7ffc
AsterixxxGallier <asterixxxgallier@aol.com>
2026-04-12 17:30:39 +0200

    Refactor and clean up code a bit

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ use crate::ray::{Ray2, Ray3};
 use crate::tiles::{TileCoordinates, TileRegion, Tiles};
 use crate::transform::TileSpacePositionAcrossTiles;
 use image::{Rgb, RgbImage};
-use indicatif::ProgressIterator;
+use indicatif::*;
 use num_traits::Float;
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
-use rayon::iter::{IndexedParallelIterator, IntoParallelIterator};
+use rayon::prelude::*;
 use std::collections::HashMap;
-use std::f64::consts::{FRAC_PI_2, PI};
+use std::f64::consts::FRAC_PI_2;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::time::Instant;
 use traversal::pixel::PixelTraversal;
@@ -83,6 +82,7 @@ pub fn rays_from(nodes: &[Node], first_node: Node) -> impl Iterator<Item = Ray3<
     })
 }
 
+#[derive(Copy, Clone)]
 pub struct TileRay {
     pub tile_coordinates: TileCoordinates,
     pub start_t: f64,
@@ -161,10 +161,29 @@ fn float_range(start: f64, end: f64, steps: usize) -> impl ExactSizeIterator<Ite
     (0..steps).map(move |index| (index as f64 + 0.5) / (steps as f64) * (end - start) + start)
 }
 
-fn _par_float_range(start: f64, end: f64, steps: usize) -> impl IndexedParallelIterator<Item = f64> {
+fn _par_float_range(
+    start: f64,
+    end: f64,
+    steps: usize,
+) -> impl IndexedParallelIterator<Item = f64> {
     (0..steps)
         .into_par_iter()
         .map(move |index| (index as f64 + 0.5) / (steps as f64) * (end - start) + start)
+}
+
+pub fn tile_rays_by_tile(
+    rays: impl Iterator<Item = Ray2<f64>>,
+) -> HashMap<TileCoordinates, Vec<(TileRay, usize)>> {
+    let mut tile_rays_by_tile: HashMap<TileCoordinates, Vec<(TileRay, usize)>> = HashMap::new();
+    for (ray_index, ray) in rays.enumerate() {
+        for tile_ray in tile_rays(ray) {
+            tile_rays_by_tile
+                .entry(tile_ray.tile_coordinates)
+                .or_default()
+                .push((tile_ray, ray_index));
+        }
+    }
+    tile_rays_by_tile
 }
 
 #[allow(unreachable_code)]
@@ -177,10 +196,9 @@ fn main() {
     };
     let mut tiles = Tiles::new();
     tiles.load_from_directory(region, "tiles");
-    // tiles.as_debug_image(region, 200.0).save("map_big.png").unwrap();
 
     let mut nodes = read_nodes("nodes.csv");
-    let prev_len = nodes.len();
+    // filter out out-of-bounds nodes
     nodes.retain(|node| {
         let position = node.position();
         let position: TileSpacePositionAcrossTiles = position.into();
@@ -189,98 +207,10 @@ fn main() {
             && position.y >= region.y_min as f64
             && position.y <= (region.y_max + 1) as f64
     });
-    println!("{} of {prev_len} retained", nodes.len());
-    /*let mut x_min = f64::INFINITY;
-    let mut x_max = f64::NEG_INFINITY;
-    let mut y_min = f64::INFINITY;
-    let mut y_max = f64::NEG_INFINITY;
-    for &node in &nodes {
-        let position = node.position();
-        let position: TileSpacePositionAcrossTiles = position.into();
-        // assert!(position.x >= 642.0, "{node:#?}\n{position:#?}");
-        // assert!(position.x <= 654.0, "{node:#?}\n{position:#?}");
-        // assert!(position.y >= 6857.0, "{node:#?}\n{position:#?}");
-        // assert!(position.y <= 6869.0, "{node:#?}\n{position:#?}");
-        x_min = x_min.min(position.x);
-        x_max = x_max.max(position.x);
-        y_min = y_min.min(position.y);
-        y_max = y_max.max(position.y);
-    }
-    println!("x_min = {x_min:>10.5}");
-    println!("x_max = {x_max:>10.5}");
-    println!("y_min = {y_min:>10.5}");
-    println!("y_max = {y_max:>10.5}");*/
 
-    /*let mut image = tiles.as_image(region, 200.0);
-    for node in nodes.iter().progress() {
-        let position: PixelSpacePositionAcrossTiles = node.position().into();
-        // dbg!(position);
-        for i in -20..=20 {
-            for j in -20..=20 {
-                let x = (position.x as i32 - region.x_min * 2000).saturating_add(i) as u32;
-                let y = (20_000 - 1 - (position.y as i32 - region.y_min * 2000)).saturating_add(j) as u32;
-                if x >= 20_000 || y >= 20_000 {
-                    continue;
-                }
-                if node.id == 55697 {
-                    image.put_pixel(x, y, Rgb([0, 0, 255]));
-                } else {
-                    image.put_pixel(x, y, Rgb([0, 255, 0]));
-                }
-            }
-        }
-    }
-    // image.save("map_big_nodes.png").unwrap();
-    // return;
-
-    // let ray_count = (nodes.len() * (nodes.len() - 1) / 2) as u64;
-    let ray_count = (nodes.len() - 1) as u64;
-    // let mut image = tiles.as_image(region, 200.0);
-    // let rays = node_rays(&nodes);
-    for mut ray in rays_from(&nodes, eiffel).progress_count(ray_count) {
-        // if rand::random_bool(0.999) {
-        //     continue;
-        // }
-        let is_free = is_line_free_across_tiles(&tiles, ray);
-        ray.start_x *= 2000.0;
-        ray.start_y *= 2000.0;
-        ray.diff_x *= 2000.0;
-        ray.diff_y *= 2000.0;
-        for segment in PixelTraversal::new(ray.as_ray_2()) {
-            let x = (segment.pixel_x - region.x_min * 2000) as u32;
-            let y = (20_000 - 1 - (segment.pixel_y - region.y_min * 2000)) as u32;
-            if is_free {
-                image.put_pixel(x, y, Rgb([0, 255, 0]));
-            } else {
-                image.put_pixel(x, y, Rgb([255, 0, 0]));
-            }
-        }
-    }
-    image.save("map_big_rays.png").unwrap();
-    return;*/
-
-    /*let rays = node_rays(&nodes);
-    let ray_count = (nodes.len() * (nodes.len() - 1) / 2) as u64;
-    dbg!(nodes.len());
-    dbg!(ray_count);
-    let start = Instant::now();
-    let free_count = rays
-        .par_bridge()
-        .progress_count(ray_count)
-        .filter(|&ray| {
-            // println!("ray: {ray:#?}");
-            // println!("is free: {}", is_line_free_across_tiles(&tiles, ray));
-            is_line_free_across_tiles(&tiles, ray)
-        })
-        .count();
-    println!("{:.3}", free_count as f64 / ray_count as f64);
-    dbg!(start.elapsed());
-    return;*/
-
-    let start_y_steps = 4000;
-    let angle_steps = 4000;
+    let start_y_steps = 1000;
+    let angle_steps = 1000;
     let mut image = RgbImage::new(start_y_steps as u32, angle_steps as u32);
-    // let mut values = Vec::with_capacity(angle_steps);
     let start = Instant::now();
     #[derive(Copy, Clone)]
     struct RayInfo {
@@ -320,32 +250,23 @@ fn main() {
                 })
         })
         .collect();
-    let mut tile_rays: HashMap<TileCoordinates, Vec<(Ray2<f64>, usize)>> =
-        HashMap::with_capacity(region.area());
-    for (ray_index, ray_info) in rays.iter().enumerate() {
-        for tile_ray in self::tile_rays(ray_info.ray) {
-            tile_rays
-                .entry(tile_ray.tile_coordinates)
-                .or_default()
-                .push((tile_ray.ray, ray_index));
-        }
-    }
+    let tile_rays = tile_rays_by_tile(rays.iter().map(|info| info.ray));
     let results = rays
         .iter()
         .map(|_| AtomicU32::default())
         .collect::<Vec<_>>();
-    region.coordinates().progress_count(region.area() as u64).for_each(|tile_coordinates| {
-        if let Some(tile_rays) = tile_rays.get(&tile_coordinates) {
+    tile_rays
+        .iter()
+        .progress()
+        .for_each(|(&tile_coordinates, tile_rays)| {
             let tile = tiles.tile(tile_coordinates).unwrap();
-            // dbg!(tile_rays.len());
             tile_rays.par_iter().for_each(|&(tile_ray, index)| {
-                let result = max_z(tile, tile_ray).unwrap();
+                let result = max_z(tile, tile_ray.ray).unwrap();
                 let result = result.to_bits();
                 results[index].fetch_max(result, Ordering::Relaxed);
             });
-        }
-    });
-    for (ray_info, result) in rays.iter().zip(results).progress_count(rays.len() as u64) {
+        });
+    for (ray_info, result) in rays.iter().zip(results) {
         let max_z = f32::from_bits(result.load(Ordering::Relaxed));
         let value = (max_z / 200.0 * 255.0).min(255.0) as u8;
         let value = Rgb([value, value, value]);
@@ -355,36 +276,6 @@ fn main() {
             value,
         );
     }
-    /*for (start_y_index, start_y) in float_range(0.0, 10.0, start_y_steps).progress().enumerate() {
-        par_float_range(-FRAC_PI_2, FRAC_PI_2, angle_steps)
-            .map(|angle| {
-                let slope = angle.tan();
-                let mut ray = Ray2 {
-                    start_x: 0.0,
-                    start_y,
-                    diff_x: 10.0,
-                    diff_y: 10.0 * slope,
-                };
-                if ray.end_y() < 0.0 {
-                    let dist_y = start_y;
-                    ray.diff_x = (-dist_y / slope).clamp(0.0, 10.0);
-                    ray.diff_y = -dist_y;
-                } else if ray.end_y() > 10.0 {
-                    let dist_y = 10.0 - start_y;
-                    ray.diff_x = (dist_y / slope).clamp(0.0, 10.0);
-                    ray.diff_y = dist_y;
-                }
-                ray.start_x += region.x_min as f64;
-                ray.start_y += region.y_min as f64;
-                let max_z = max_z_across_tiles(&tiles, ray);
-                (max_z / 200.0 * 255.0).min(255.0) as u8
-            })
-            .collect_into_vec(&mut values);
-        for (index, &value) in values.iter().enumerate() {
-            let value = Rgb([value, value, value]);
-            image.put_pixel(start_y_index as u32, index as u32, value);
-        }
-    }*/
 
     let elapsed = start.elapsed();
     // let num_rays = image.width() * image.height();
@@ -396,72 +287,4 @@ fn main() {
         num_rays as f64 / elapsed.as_secs_f64() / 1e6,
     );
     image.save("out_big.png").unwrap();
-    return;
-
-    for coordinates in region.coordinates().progress_count(100) {
-        let x_size = 2000;
-        let y_size = 2000;
-        let z_size = 200;
-
-        let map = tiles.tile(coordinates).unwrap();
-
-        let start_y_resolution = 1;
-        let angle_resolution = 1;
-        let mut image = RgbImage::new(
-            (y_size * start_y_resolution) as u32,
-            (y_size * angle_resolution) as u32 - 1,
-        );
-        let mut values = Vec::with_capacity(y_size * angle_resolution - 1);
-        let start = Instant::now();
-        let progress_bar = indicatif::ProgressBar::new((y_size * start_y_resolution) as u64);
-        for start_y_index in 0..y_size * start_y_resolution {
-            progress_bar.inc(1);
-            let start_y = (start_y_index as f64 + 0.5) / start_y_resolution as f64;
-            (1..y_size * angle_resolution)
-                .into_par_iter()
-                .map(|angle_index| {
-                    let angle =
-                        (angle_index as f64 / angle_resolution as f64 / y_size as f64 - 0.5) * PI;
-                    let slope = angle.tan();
-                    let mut ray = Ray2 {
-                        start_x: 0.0,
-                        start_y,
-                        diff_x: x_size as f64,
-                        diff_y: y_size as f64 * slope,
-                    };
-                    if ray.end_y() < 0.0 {
-                        let dist_y = start_y;
-                        ray.diff_x = (-dist_y / slope).clamp(0.0, y_size as f64);
-                        ray.diff_y = -dist_y;
-                    } else if ray.end_y() > y_size as f64 {
-                        let dist_y = y_size as f64 - start_y;
-                        ray.diff_x = (dist_y / slope).clamp(0.0, y_size as f64);
-                        ray.diff_y = dist_y;
-                    }
-                    let max_z = max_z(&map, ray).unwrap();
-                    (max_z / z_size as f32 * 255.0).min(255.0) as u8
-                })
-                .collect_into_vec(&mut values);
-            for (index, &value) in values.iter().enumerate() {
-                let value = Rgb([value, value, value]);
-                image.put_pixel(start_y_index as u32, index as u32, value);
-            }
-        }
-        progress_bar.finish_and_clear();
-
-        let elapsed = start.elapsed();
-        let num_rays = image.width() * image.height();
-        println!(
-            "{} rays computed in {:?} ({:.2} million rays per second)",
-            num_rays,
-            elapsed,
-            num_rays as f64 / elapsed.as_secs_f64() / 1e6,
-        );
-        image
-            .save(format!(
-                "out/out_{:0>4}_{:0>4}.png",
-                coordinates.x, coordinates.y
-            ))
-            .unwrap();
-    }
 }
