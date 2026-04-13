0c38b94916016c774a8814d780a1bb6aeebb1471
AsterixxxGallier <asterixxxgallier@aol.com>
2026-04-13 16:35:52 +0200

    Optimize Map

--- src/map.rs
+++ src/map.rs
@@ -1,22 +1,22 @@
+use image::{ImageBuffer, Rgb};
 use rayon::iter::ParallelIterator;
+use rayon::prelude::{IndexedParallelIterator, IntoParallelIterator};
 use std::fs::File;
 use std::mem;
 use std::path::Path;
-use image::{ImageBuffer, Rgb};
-use rayon::prelude::IntoParallelIterator;
 use tiff::decoder::{Decoder, DecodingResult};
 
 /// A two-dimensional array of values.
 #[derive(Debug)]
 pub struct Map<T> {
-    store: Box<[T]>,
+    store: Vec<T>,
     x_len: usize,
     y_len: usize,
 }
 
 impl<T: Copy + Default> Map<T> {
     pub fn new(x_len: usize, y_len: usize) -> Self {
-        let store = vec![T::default(); x_len * y_len].into_boxed_slice();
+        let store = vec![T::default(); x_len * y_len];
         Self {
             store,
             x_len,
@@ -24,44 +24,45 @@ impl<T: Copy + Default> Map<T> {
         }
     }
 
-    pub fn from_vec(
-        x_len: usize,
-        y_len: usize,
-        vec: Vec<T>,
-    ) -> Self {
+    pub fn from_vec(x_len: usize, y_len: usize, vec: Vec<T>) -> Self {
         assert_eq!(vec.len(), x_len * y_len);
         Self {
-            store: vec.into_boxed_slice(),
+            store: vec,
             x_len,
             y_len,
         }
     }
 
-    pub fn from_fn(
-        x_len: usize,
-        y_len: usize,
-        f: impl Fn(usize, usize) -> T + Sync,
-    ) -> Self where T: Send {
+    pub fn from_fn(x_len: usize, y_len: usize, f: impl Fn(usize, usize) -> T + Sync) -> Self
+    where
+        T: Send,
+    {
         Self {
-            store: (0..x_len * y_len).into_par_iter().map(|index| {
-                let x_index = index % x_len;
-                let y_index = y_len - 1 - index / x_len;
-                f(x_index, y_index)
-            }).collect(),
+            store: (0..x_len * y_len)
+                .into_par_iter()
+                .map(|index| {
+                    let x_index = index % x_len;
+                    let y_index = y_len - 1 - index / x_len;
+                    f(x_index, y_index)
+                })
+                .collect(),
             x_len,
             y_len,
         }
     }
 
-    pub fn regenerate_from_fn(
-        &mut self,
-        f: impl Fn(usize, usize) -> T,
-    ) {
-        for y_index in 0..self.y_len {
-            for x_index in 0..self.x_len {
-                self.set(x_index, y_index, f(x_index, y_index));
-            }
-        }
+    pub fn regenerate_from_fn(&mut self, f: impl Fn(usize, usize) -> T + Sync)
+    where
+        T: Send,
+    {
+        (0..self.x_len * self.y_len)
+            .into_par_iter()
+            .map(|index| {
+                let x_index = index % self.x_len;
+                let y_index = self.y_len - 1 - index / self.x_len;
+                f(x_index, y_index)
+            })
+            .collect_into_vec(&mut self.store);
     }
 
     pub fn x_len(&self) -> usize {
@@ -107,7 +108,7 @@ impl Map<f32> {
     pub fn regenerate_from_tiff(&mut self, file: File) {
         let reader = std::io::BufReader::new(file);
         let mut decoder = Decoder::new(reader).unwrap();
-        let mut vec = mem::take(&mut self.store).into_vec();
+        let mut vec = mem::take(&mut self.store);
         vec.clear();
         let mut data = DecodingResult::F32(vec);
 
@@ -117,7 +118,7 @@ impl Map<f32> {
             panic!()
         };
 
-        self.store = data.into_boxed_slice();
+        self.store = data;
     }
 
     pub fn save_as_image(&self, white_value: f32, path: impl AsRef<Path>) {
@@ -130,8 +131,7 @@ impl Map<f32> {
             if value >= white_value {
                 Rgb([255, 0, 0])
             } else {
-                let value_u8 =
-                    ((value / white_value).clamp(0.0, 1.0) * 255.0) as u8;
+                let value_u8 = ((value / white_value).clamp(0.0, 1.0) * 255.0) as u8;
                 Rgb([value_u8, value_u8, value_u8])
             }
         })
