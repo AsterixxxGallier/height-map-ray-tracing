b536c1c246c1171f50bbbb7e3093801b0bef46ed
Leonard Nolting <leonolting@gmail.com>
2026-05-19 18:12:42 +0200

    set default link length to 10km

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Args {
     /// Maximum link length
-    #[arg(short, long, default_value_t = f64::INFINITY)]
+    #[arg(short, long, default_value_t = 10_000.0)]
     pub max_link_length: f64,
 }
